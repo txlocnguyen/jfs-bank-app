1702a4b656f6104b662428e0f0a652f13fee6607
Loc Nguyen <txlocnguyen@gmail.com>
2022-02-15 16:15:19 -0600

    define constructor and get methods

--- src/com/bank_app/BankAccount.java
+++ src/com/bank_app/BankAccount.java
@@ -1,9 +1,21 @@
 package com.bank_app;
 
 public class BankAccount {
-    double checkingBalance = 0.00;
-    double savingsBalance = 0.00;
-    final double checkingOpeningDeposit;
-    final double savingsOpeningDeposit;
-    
+    private double checkingBalance = 0.00;
+    private double savingsBalance = 0.00;
+    final private double checkingOpeningDeposit;
+    final private double savingsOpeningDeposit;
+    public BankAccount(double checkingOpeningDeposit, double savingsOpeningDeposit){
+        this.checkingOpeningDeposit = checkingOpeningDeposit;
+        this.savingsOpeningDeposit = savingsOpeningDeposit;
+        this.checkingBalance += checkingOpeningDeposit;
+        this.savingsBalance += savingsOpeningDeposit;
+    }
+
+    public double getCheckingBalance() {
+        return this.checkingBalance;
+    }
+    public double getSavingsBalance(){
+        return this.savingsBalance;
+    }
 }
--- src/com/bank_app/BankApp.java
+++ src/com/bank_app/BankApp.java
@@ -2,6 +2,8 @@ package com.bank_app;
 
 public class BankApp {
     public static void main(String[] args){
-
+        BankAccount customerAcct = new BankAccount(250, 500);
+        System.out.println(customerAcct.getCheckingBalance());
+        System.out.println(customerAcct.getSavingsBalance());
     }
 }
