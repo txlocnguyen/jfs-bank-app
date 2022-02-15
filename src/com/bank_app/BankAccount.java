3605b15e5340cfca0e2aed1793e4e4858f800ff1
Loc Nguyen <txlocnguyen@gmail.com>
2022-02-15 16:04:28 -0600

    initial commit

---
+++ src/com/bank_app/BankAccount.java
@@ -0,0 +1,9 @@
+package com.bank_app;
+
+public class BankAccount {
+    double checkingBalance = 0.00;
+    double savingsBalance = 0.00;
+    final double checkingOpeningDeposit;
+    final double savingsOpeningDeposit;
+    
+}
---
+++ src/com/bank_app/BankApp.java
@@ -0,0 +1,7 @@
+package com.bank_app;
+
+public class BankApp {
+    public static void main(String[] args){
+
+    }
+}
